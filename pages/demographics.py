2e794cdb1d899a6726c433fb200ae993e2725dba
canonicalized <office@canonicalized.com>
2023-04-18 01:24:38 +0300

    Add files via upload

---
+++ pages/demographics.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import numpy as np
+import snowflake.connector
+import plotly.graph_objs as go
+import plotly.express as px
+import streamlit as st
+
+from st_pages import Page, show_pages
+# Specify what pages should be shown in the sidebar, and what their titles and icons should be
+show_pages(
+    [
+        Page("locations.py", "Locations", "📍"),
+        Page("pages/demographics.py", "Demographics", "📊"),
+        Page("pages/locate_nearby.py", "Locate Nearby", "🌍"),
+    ]
+)
+
+st.set_page_config(
+    layout="wide",
+    page_title="Demographics",
+    page_icon="📊"
+)
+
+st.title("Demographics")
+st.markdown("Let's see the income demographics of people living in neighborhoods with at least one Starbucks location as opposed to those that don't have any")
+
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    return pd.read_sql(query, conn)
+
+df = run_query("SELECT * from DORIAN.PUBLIC.STARBUCKS_DEMOGRAPHICS;")
+
+
+with st.container():
+    col1, col2 = st.columns(2)
+
+    with col1:
+        # Filter the data by rows where Breakdown is null
+        null_data = df[df["Breakdown"].isnull()]
+
+        null_data["Starbucks in neighborhood"] = null_data["Starbucks in neighborhood"].map({True: "Starbucks in neighborhood", False: "Starbucks not in neighborhood"})
+
+        # Group the data by Starbucks in neighborhood and calculate the sum of the Population column
+        grouped_data = null_data.groupby("Starbucks in neighborhood").sum()
+
+        # Create the pie chart using plotly.express
+        fig = px.pie(grouped_data, values="Value", names=grouped_data.index, hole=0.5, color=grouped_data.index,
+                    color_discrete_map={"Starbucks in neighborhood": "#036635", "Starbucks not in neighborhood": "#BAB0AC"})
+
+        # Update the layout of the chart
+        fig.update_layout(
+            title="Total Population by Starbucks in Neighborhood",
+            font=dict(size=12),
+            height=400,
+            width=400
+        )
+
+        # Show the chart
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True, config={'displayModeBar': False})
+
+    with col2:
+        col21, col22 = st.columns(2)
+        with col21:
+            st.text("")
+            st.markdown("The pie chart shows what percentage of people live in areas with a Starbucks nearby.")
+            st.markdown("The bottom chart compares the percentage of people with different income levels who have access to a Starbucks in their neighborhood (green) versus those who don't (gray).")
+
+        with col22:
+            st.text("")
+            st.markdown("It becomes clear that Starbucks coffee shops are less common in lower-income areas and more prevalent in higher-income neighborhoods.")
+            st.markdown("To come up with this analysis, we used US Census data to match up the Starbucks location data by census block group.")
+        
+
+
+
+
+grouped = df.groupby(["Starbucks in neighborhood","Breakdown"])["Value"].sum()
+pot = pd.concat([grouped / grouped.groupby("Starbucks in neighborhood").transform("sum"), grouped.rename("Population")],axis=1)
+pot = pot.reset_index()
+pot["Breakdown Order"] = pot["Breakdown"].str.extract("(\d+)").astype(int)
+pot["Breakdown Order"] = pot["Breakdown"].apply(lambda x: 0 if x == "Less than $10 000" else int(x.replace("$", "").replace(",", "").split(" ")[0]))
+pot = pot.sort_values(by="Breakdown Order")
+pot["Population"] = pot["Population"].astype(str) 
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(
+        x=pot[pot["Starbucks in neighborhood"] == False]["Breakdown"],
+        y=pot[pot["Starbucks in neighborhood"] == False]["Value"]*100,
+        mode="lines+markers",
+        name="Starbucks not in neighborhood",
+        line_color='#BAB0AC',
+        hovertemplate='<b>Income level:</b> %{x}<br><b>Population % of total from neighborhoods without Starbucks:</b> %{y:.1f}%'
+    )
+)
+
+fig.add_trace(
+    go.Scatter(
+        x=pot[pot["Starbucks in neighborhood"] == True]["Breakdown"],
+        y=pot[pot["Starbucks in neighborhood"] == True]["Value"]*100,
+        mode="lines+markers",
+        name="Starbucks in neighborhood",
+        fill='tonexty', # fill area between trace0 and trace1
+        line_color='#036635',
+        hovertemplate='<b>Income level:</b> %{x}<br><b>Population % of total from neighborhoods with Starbucks:</b> %{y:.1f}%'
+    )
+)
+
+# Update the layout of the chart
+fig.update_layout(
+    title="Starbucks presence in neighborhoods by Household Income Level",
+    xaxis_title="",
+    yaxis_title="Percentage of Total Value",
+    legend_title="Starbucks in neighborhood",
+    font=dict(size=12),
+    height=600,
+    width=800,
+    xaxis=dict(showticklabels=False)
+)
+
+
+# Show the chart
+st.plotly_chart(fig, theme="streamlit", use_container_width=True, config={'displayModeBar': False})
+
---
+++ pages/locate_nearby.py
@@ -0,0 +1,180 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+# import osmnx as ox
+from shapely.geometry import Point, Polygon
+import openrouteservice as ors
+import openrouteservice.exceptions as ors_exc
+from geopy.geocoders import Nominatim
+
+from st_pages import Page, show_pages
+# Specify what pages should be shown in the sidebar, and what their titles and icons should be
+show_pages(
+    [
+        Page("locations.py", "Locations", "📍"),
+        Page("pages/demographics.py", "Demographics", "📊"),
+        Page("pages/locate_nearby.py", "Locate Nearby", "🌍"),
+    ]
+)
+st.set_page_config(
+    layout="wide",
+    page_title="Locator",
+    page_icon="🌍"
+)
+st.title("Find a Starbucks Nearby")
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    return pd.read_sql(query, conn) 
+
+df = run_query("SELECT PLACEKEY, PARENT_PLACEKEY, LATITUDE, LONGITUDE, STREET_ADDRESS, CITY, REGION, POSTAL_CODE from CORE_POI;")
+df["CITY_STATE"] = df["CITY"] + ", " + df['REGION']
+
+with st.expander("Input your address and we'll find the closest coffee shop(s) to your location", expanded=True):
+    with st.container():
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            # Set up input form
+            address = st.text_input("Enter your address:", placeholder="19 Washington Square N, New York, NY 10011, USA")
+
+        with col2:
+            transit_options = ['Walk','Drive','Cycle']
+            transit_selected = st.selectbox("Walk or drive?", transit_options)
+
+        with col3:
+            # Set up walk/drive time selection
+            time_options = [5, 10, 15]  # in minutes
+            time_selected = st.selectbox("Select walk/drive time (minutes):", time_options)
+        
+        # Add apply button
+        fas = st.button('Find a Starbucks')
+
+def mode_transit(transit_selected):
+    if transit_selected == 'Walk':
+        return 'foot-walking'
+    elif transit_selected == 'Drive':
+        return 'driving-car'
+    elif transit_selected == 'Cycle':
+        return 'cycling-regular'
+    else:
+        return 'foot-walking'
+
+# m = folium.Map(location=(-73.9975615903237, 40.73192615), zoom_start=13)
+# st_folium(map, width=700, height=450)
+if fas:
+    if len(address)<5:
+        st.warning("Please input a valid address")    
+    else:
+        with st.spinner('Generating map...'):
+            # Calculate user location coordinates using geopy
+            geolocator = Nominatim(user_agent="my_app")
+            location = geolocator.geocode(address)
+            if location is None:
+                st.error("Address not found.")
+                st.stop()
+            user_coords = (location.longitude,location.latitude)
+
+            # st.write(user_coords)
+
+            # Define function to generate isochrone
+            def generate_isochrone(location, time, mode=transit_selected):
+                try: 
+                    # Set up OpenRouteService client with API key
+                    client = ors.Client(key=st.secrets.ors_creds.api_key)
+
+                    # Specify query parameters
+                    query = {
+                        "locations": [location],
+                        "range": [0, time],
+                        "range_type": "time",
+                        "profile": mode_transit(mode),
+                        "location_type": "start"
+                    }
+
+                    # Send request to OpenRouteService API
+                    result = client.isochrones(**query)
+                    
+                    # st.write(result)
+
+                    # Extract isochrone geometry
+                    polygons = [Polygon(shape) for shape in result["features"][0]["geometry"]["coordinates"]]
+                    # feature = result["features"][0]
+                    return polygons#, feature
+                except ors_exc.ApiError:
+                    st.error("Unable to generate isochrone. Please try again later.")
+                    st.stop()
+
+            # Generate isochrone
+            isochrone = generate_isochrone(user_coords, time_selected * 60, mode="walking")
+
+        
+            sb_locations = pd.DataFrame()
+            # Define layer for Starbucks locations within isochrone
+            for _, row in df.iterrows():
+                sb_coords = (row["LONGITUDE"], row["LATITUDE"])
+                sb_point = Point(sb_coords)
+                if any(sb_point.within(polygon) for polygon in isochrone):
+                    sb_locations = sb_locations.append({"LONGITUDE": row["LONGITUDE"], "LATITUDE": row["LATITUDE"], "STREET_ADDRESS":row["STREET_ADDRESS"]}, ignore_index=True)
+            
+            st.pydeck_chart(pdk.Deck(
+                map_style=None,
+                initial_view_state=pdk.ViewState(
+                    longitude=location.longitude,
+                    latitude=location.latitude,
+                    zoom=13,
+                    pitch=0,
+                    bearing=0
+                ),
+                layers=[
+                    pdk.Layer(
+                        'ScatterplotLayer',
+                        data=sb_locations,
+                        get_position='[LONGITUDE, LATITUDE]',
+                        get_color='[0, 112, 74, 160]',
+                        get_radius=200,
+                        pickable=True,
+                        auto_highlight=True,
+                        filled=True,
+                        radius_scale=2,
+                        radius_min_pixels=1,
+                        radius_max_pixels=8,
+                        line_width_min_pixels=1,
+                    ),
+
+                    # pdk.Layer(
+                    #     "PolygonLayer",
+                    #     data=json.dumps(feature),
+                    #     get_polygon="geometry.coordinates",
+                    #     filled=True,
+                    #     stroked=True,
+                    #     extruded=False,
+                    #     get_fill_color="[255, 255, 255, 150]",
+                    #     get_line_color="[255, 255, 255]",
+                    #     line_width_min_pixels=1,
+                    # ),
+                    pdk.Layer(
+                        "ScatterplotLayer",
+                        data=[{"position": user_coords}],
+                        get_position="position",
+                        get_color="[255, 0, 0, 255]",
+                        get_radius=100,
+                    )
+                    
+                ],
+                tooltip = {
+                    "text": "Address: {STREET_ADDRESS}"
+                }
+            ))
